0e2a539c41ecbd019df33ea212d44fb4745f3af6
Hans Mattin-Lassei <hans.mattin-lassei@epndata.com>
2022-02-28 23:31:59 +0100

    lektion 9 (html, css, asp.net mvc, adobe xd)

---
+++ lektion-9/WebApp/Controllers/ContactController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult BookMeeting()
+        {
+            return View();
+        }
+    }
+}
